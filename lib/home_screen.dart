ffe17a941e72ffb181749d81794abcf2df48e0a4
TZONE TECH <tzonetech8@gmail.com>
2026-07-12 14:14:00 +0300

    Remove unnecessary blank line in home_screen.dart

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'auth_provider.dart';
-
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
